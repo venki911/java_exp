b6e2deb31025c6dca451bd3a0b5a654966b5e243
venki911 <gvenki911@gmail.com>
2017-02-20 15:45:46 +0530

    Create ForEach.java

---
+++ ForEach.java
@@ -0,0 +1,17 @@
+class ForEach {
+    
+    
+    static int arr[] =  {3,2,3,3};
+    
+    public static void main(String args[]){
+     
+      System.out.println("Array length is "+ arr.length);
+      for(int i : arr){
+        System.out.println(i);
+      }   
+     
+        
+    }
+    
+    
+}
